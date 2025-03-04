123fcc7d48fb2c81d88182c8a80e53100f9a31f3
rim <127942413+taerim0@users.noreply.github.com>
2025-03-04 08:52:57 +0000

    initial design

--- web/src/pages/main/MainPage.tsx
+++ web/src/pages/main/MainPage.tsx
@@ -4,17 +4,22 @@ import './MainPage.css';
 import Logo from './tempLogozz.png';
 
 const MainPage = () => {
+    const navigation = useNavigate();
+
     return (
         <div className="MainContainer">
+            <div className="LogoBackground">
+                
+            </div>
             <div className="LogoBox">
                 <img src={Logo} className='Logo' />
             </div>
             <div className="LoginBox">
-                <div className="SignIn">
+                <div className="SignIn" onClick={()=>{navigation('/auth')}}>
                     <a>로그인쓰</a>
                 </div>
-                <div className="SignUp">
-                    <a>록아웄쓰</a>
+                <div className="SignUp" onClick={()=>{navigation('/auth')}}>
+                    <a>횐가입쓰</a>
                 </div>
             </div>
             <div className="ContentsBox">코딩공부한걸기록하고공유할수있는싸이트가있따?뿌슝빠슝슝너무쩔잖아반드시이용해야겠는걸~~~</div>
