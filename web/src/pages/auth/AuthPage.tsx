e970506ffe9e0a1bfefa66e233c87163e65c8432
rim <127942413+taerim0@users.noreply.github.com>
2025-03-23 11:52:12 +0000

    gogogo

--- web/src/pages/auth/AuthPage.tsx
+++ web/src/pages/auth/AuthPage.tsx
@@ -1,8 +1,27 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 
 const AuthPage = () => {
+    const navigation = useNavigate();
+    const location = useLocation();
+
+    const mode = location.state;
+
+    const [ID, setID] = useState();
+    const [PW, setPW] = useState();
+    const [PWC, setPWC] = useState();
+
+    useEffect(() => {
+        fetch("https://localhost:3000/api/auth/auth_check", {
+            credentials: 'include',
+        })
+        .then((res) => {res.json})
+        .then((json) => {
+            
+        })
+    })
+
     return (
         <div>
             디스 이즈 로긴
--- web/src/pages/main/MainPage.tsx
+++ web/src/pages/main/MainPage.tsx
@@ -4,7 +4,64 @@ import './MainPage.css';
 import Logo from './tempLogozz.png';
 
 const MainPage = () => {
+    const [AccountState, setAccountState] = useState<String>();
     const navigation = useNavigate();
+
+    useEffect(() => {
+        fetch("https://localhost:3000/api/auth/auth_check", {
+            credentials : 'include',
+        })
+        .then((res) => res.json())
+        .then((json) => {
+            if (json.isLoggedIn === true)
+                setAccountState("Logged In")
+            else setAccountState("Logged Out");
+        })
+    })
+
+    if (AccountState === "Logged In") {
+        return (
+            <div className="MainContainer">
+                <div className="LogoBackground">
+                    
+                </div>
+                <div className="LogoBox">
+                    <img src={Logo} className='Logo' />
+                </div>
+                <div className="LoginBox">
+                    <div className="SignIn" onClick={()=>{navigation('/auth')}}>
+                        <a>로그아웄</a>
+                    </div>
+                    <div className="SignUp" onClick={()=>{navigation('/auth')}}>
+                        <a>로그아웄</a>
+                    </div>
+                </div>
+                <div className="ContentsBox">로그인 상태입니다.</div>
+            </div>
+        )
+    }
+
+    if (AccountState === "Logged Out") {
+        return (
+            <div className="MainContainer">
+                <div className="LogoBackground">
+                    
+                </div>
+                <div className="LogoBox">
+                    <img src={Logo} className='Logo' />
+                </div>
+                <div className="LoginBox">
+                    <div className="SignIn" onClick={()=>{navigation('/auth', {state: "Log In"})}}>
+                        <a>로그인쓰</a>
+                    </div>
+                    <div className="SignUp" onClick={()=>{navigation('/auth', {state: "Register"})}}>
+                        <a>횐가입쓰</a>
+                    </div>
+                </div>
+                <div className="ContentsBox">코딩공부한걸기록하고공유할수있는싸이트가있따?뿌슝빠슝슝너무쩔잖아반드시이용해야겠는걸~~~</div>
+            </div>
+        )
+    }
 
     return (
         <div className="MainContainer">
@@ -15,10 +72,10 @@ const MainPage = () => {
                 <img src={Logo} className='Logo' />
             </div>
             <div className="LoginBox">
-                <div className="SignIn" onClick={()=>{navigation('/auth')}}>
+                <div className="SignIn" onClick={()=>{navigation('/auth', {state: "Log In"})}}>
                     <a>로그인쓰</a>
                 </div>
-                <div className="SignUp" onClick={()=>{navigation('/auth')}}>
+                <div className="SignUp" onClick={()=>{navigation('/auth', {state: "Register"})}}>
                     <a>횐가입쓰</a>
                 </div>
             </div>
